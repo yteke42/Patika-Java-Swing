2d2e00dc8de8dd92cbf8e9975171b568e8e74854
Yunusteke <yunusteke4646@gmail.com>
2025-07-16 01:06:39 +0300

    up

--- PatikaCustomer/src/App.java
+++ PatikaCustomer/src/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Helper.setTheme();
+        @SuppressWarnings("unused")
         LoginUI loginUI = new LoginUI();
 
     }
